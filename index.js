bb8300fe0f0cde71ebe725563aecc850f9dfcd44
Ben <ben.liu@kmart.com.au>
2022-02-12 09:51:32 +1100

    feat: 🎸 skeleton

---
+++ index.js
@@ -0,0 +1,12 @@
+const getConsolidatedCatelog = () => {
+  const result = [];
+
+  // Put all catelog items within A into result
+  // ...
+
+  // Go through all catelog items one by one within B to compare with each item of A
+  // Ignore it if a duplicate. otherwise, add it into final result
+  // if B1 a duplicate of A1? => isDuplicate(A1, B1)
+};
+
+getConsolidatedCatelog();
---
+++ models/barcodes.js
@@ -0,0 +1,9 @@
+const csvtojsonV1 = require("csvtojson/v1");
+
+class Barcodes {
+  isHaveSameBarcode(skuFromA, skuFromB) {
+    // get all barcodes of skuFromA
+    // get all barcodes of skuFromB
+    // see if there is any same barcode
+  }
+}
